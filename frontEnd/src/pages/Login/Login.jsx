05980d41a8c0bdbd106b983e0adecdbeec5e7bc9
sMv-Jones <mohd.cryengine@gmail.com>
2025-07-30 19:38:20 +0530

    login

--- frontEnd/src/pages/Login/Login.jsx
+++ frontEnd/src/pages/Login/Login.jsx
@@ -1,20 +1,18 @@
-import "./login.css"
+import "./login.css";
+
 function Login() {
-     return (
-        <div>
-            <div>
-                <div >
-                    <img src="/images/border.png" width="30%"/>
-                </div>
-                <form>
-                    <h1>Welcome <br /> Back!</h1>
-                    <input type="text" required placeholder="Enter username" title="" />
-                    <input type="password" required placeholder="Enter password" title="" />
-                    <button type="submit" id="submit">Submit</button>
-                </form>
-            </div>
-        </div>
-    );
+  return (
+    <div className="login-wrapper">
+      <div className="login-box">
+        <form className="login-form">
+          <h1>Welcome <br /> Back!</h1>
+          <input type="text" required placeholder="Enter username" />
+          <input type="password" required placeholder="Enter password" />
+          <button type="submit" id="submit">Submit</button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
