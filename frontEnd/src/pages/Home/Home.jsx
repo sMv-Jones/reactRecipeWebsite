8f8f7fe326f592521807fbf42a2934e2a0f51108
sMv-Jones <mohd.cryengine@gmail.com>
2025-07-30 19:12:34 +0530

    home page

--- frontEnd/src/pages/Home/Home.jsx
+++ frontEnd/src/pages/Home/Home.jsx
@@ -1,7 +1,11 @@
-function Home(){
+import "./Home.css"
+function Home() {
     return (
-        <div>
-            This is home
+        <div className="home-container">
+            <h1 className="home-title">Welcome to Afzal's Recipe Website</h1>
+            <p className="home-description">
+                Discover a world of delicious recipes, cooking tips, and kitchen inspiration. Whether you're a beginner or a seasoned chef, there's something here for everyone.
+            </p>
         </div>
     )
 }
